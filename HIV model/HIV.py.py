5bc827814c2d78d8bdff7f37e304f4827663a367
Omisola Idowu Paul <57432464+omisolaidowu@users.noreply.github.com>
2020-06-23 05:57:24 +0100

    Update HIV.py.py

--- HIV model/HIV.py.py
+++ HIV model/HIV.py.py
@@ -10,7 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
-df=pd.read_excel(r'C:\Users\Paul\Desktop\Python projects\HIV_3.xlsx')
+df=pd.read_excel(r'HIV_3.xlsx')
 print(df)
 
 print(df.isnull().sum())
